66b978399e02d2d91cf586883214e90ed8ae12a6
Axel Luna Hernandez <axel.lu04@gmail.com>
2025-11-10 04:04:55 -0600

    Añadir .gitignore y limpiar carpeta target

--- target/classes/db/migration/V2__Alter_columna_contrasenia_a_100.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE [dbo].[Administrador] ALTER COLUMN contrasenia VARCHAR(100) NOT NULL;
-ALTER TABLE [dbo].[Cliente] ALTER COLUMN contrasenia VARCHAR(100) NOT NULL;
-ALTER TABLE [dbo].[Vendedor] ALTER COLUMN contrasenia VARCHAR(100) NOT NULL;
